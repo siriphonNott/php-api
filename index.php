2ee49b9c7415dd7d67497cbfff3efb02d0681af0
siriphonnot <siriphonnot@gmail.com>
2019-01-30 01:53:31 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php 
+/*
+  project: Simple Rest API with PHP
+  author: NottDev
+  created at: 29/01/19
+*/
+
+//get http request method
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+
+//get Content-Type from http request header
+$contentType = $_SERVER["CONTENT_TYPE"];
+
+//set dynamic response Content-Type
+$responseContentType = explode(";" , $contentType)[0];
+
+//import SimpleRest Class
+require('SimpleRest.php');
+$rest = new SimpleRest($responseContentType);
+
+//Set default http status code = 200
+$httpStatusCode = 200; 
+
+//get response
+$body['Content-Type'] = $contentType;
+
+//Check http Method
+switch ($httpMethod) {
+  case 'GET':
+    $body['data'] = $_GET;
+    break;
+
+  case 'POST':
+    
+    //Content-Type is form-data or x-www-form-urlencoded
+    if( ( strpos($contentType,"multipart/form-data") !== false) || (strpos($contentType,"application/x-www-form-urlencoded" ) !== false )) {
+      $input = $_POST;
+    } else {
+      $inputJSON = file_get_contents('php://input');
+      $input = json_decode($inputJSON , true); //convert JSON to array or StdClass Object
+    }
+    $body['data'] = $input;
+    
+  break;
+
+  case 'DELETE':
+    //TO DO ..
+    break;
+
+    case 'PUT':
+    //TO DO ..
+    break;
+
+  default:
+    # code...
+    break;
+}
+
+//Response data
+$rest->setHttpStatus($httpStatusCode);
+echo $rest->response($body['data']);
+
+exit();
