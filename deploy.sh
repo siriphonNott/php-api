f1162af9b08b665bee73599683232945bcf461a1
siriphonnot <siriphonnot@gmail.com>
2019-02-15 18:50:07 +0700

    del deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R $(HOME)/clone /projects/app01"
+# lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R ${HOME}/clone/ /projects/app01"
